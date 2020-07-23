c22f286bfa5771a38e533d1cfc2da3c814f273ce
Zaineb Nawaz <zaineb.nawaz01@gmail.com>
2020-07-23 22:10:58 +0500

    Styling on Inputs

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -5,8 +5,18 @@ class NewTransaction extends StatelessWidget {
   final amountController = TextEditingController();
 
   final Function addTx;
-
   NewTransaction(this.addTx);
+
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTx(enteredTitle, enteredAmount);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,16 +30,21 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (value) {
+                submitData();
+              },
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (value) {
+                submitData();
+              },
             ),
             FlatButton(
               child: Text('Add Transaction'),
-              onPressed: () => {
-                addTx(titleController.text, double.parse(amountController.text))
-              },
+              onPressed: () => {submitData},
               textColor: Colors.purple,
             ),
           ],
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -24,7 +24,8 @@ class TransactionList extends StatelessWidget {
                       decoration: BoxDecoration(
                           border: Border.all(color: Colors.purple, width: 2)),
                       padding: EdgeInsets.all(10),
-                      child: Text('\$${transactions[index].amount}',
+                      child: Text(
+                          '\$${transactions[index].amount.toStringAsFixed(2)}',
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
